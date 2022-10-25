d93a558a732205dd1fb06cf99f332295bde2ec07
rhldkszns97 <sujeng98@naver.com>
2022-10-26 00:19:12 +0900

    2016년

---
+++ src/coplit/dailyCoding/Programmers/기타/Lv1/이천십육년.java
@@ -0,0 +1,17 @@
+package coplit.dailyCoding.Programmers.기타.Lv1;
+
+public class 이천십육년 {
+    public String solution(int a, int b) {
+        String[] day = {"THU","FRI","SAT","SUN","MON","TUE","WED"};
+        int[] Month = {31,29,31,30,31,30,31,31,30,31,30,31};
+        int date = 0;
+
+        for (int i=0; i<a-1; i++) {
+            date += Month[i];
+        }
+
+        date += b;
+
+        return day[date%7];
+    }
+}
