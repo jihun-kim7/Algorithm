78eae843dee06594adeaa508d5d2a6ff06a17633
rhldkszns97 <sujeng98@naver.com>
2022-11-09 05:20:32 +0900

    트리나누기

--- src/coplit/dailyCoding/Programmers/기타/Lv1/트리나누기.java
+++ src/coplit/dailyCoding/Programmers/기타/Lv1/트리나누기.java
@@ -3,25 +3,25 @@ package coplit.dailyCoding.Programmers.기타.Lv1;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class 트리나누기 {
     /**
      1. 그래프마다 간선저장
      2. 노드에서 이어진 노드의 개수가 n//3 이면 체크(정답 배열에 저장)
      */
+    static ArrayList<Integer> answer = new ArrayList<>();
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        System.out.println(solution());
     }
 
     public static String solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<ArrayList<int[]>> list = new ArrayList<>();
         Map<Integer, Integer> map = new HashMap<>();
+//        ArrayList<Integer> answer = new ArrayList<>();
 
         int n = Integer.parseInt(br.readLine());
 
@@ -33,20 +33,36 @@ public class 트리나누기 {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-
-            list.get(a).add(b);
-            list.get(b).add(a);
+            int[] temp = new int[]{a,i};
+            int[] temp2 = new int[]{b,i};
+            list.get(a).add(temp2);
+            list.get(b).add(temp);
         }
 
+        dfs(list,map,0,0,n/3,answer);
+        Collections.sort(answer);
+
+        return answer.stream().map(String::valueOf).collect(Collectors.joining(" "));
     }
 
-    public static void dfs(ArrayList<ArrayList<Integer>> list, Map<Integer, Integer> map, int pre, int now) {
-        map.put(now, map.getOrDefault(now,0) + 1);
+    public static int dfs(ArrayList<ArrayList<int[]>> list, Map<Integer, Integer> map, int pre, int now, int want, ArrayList<Integer> answer) {
 
         for (int i=0; i<list.get(now).size(); i++) {
-            if (pre == now) continue;
-            dfs(list,map,now,i);
+            int[] arr = list.get(now).get(i);
+            int next = arr[0];
+            if (next == pre) continue;
+            int edge = arr[1];
+            int result = dfs(list,map,now,next,want,answer);
+            if (result == want) {
+                answer.add(edge);
+                System.out.println(edge);
+                continue;
+            }
+            map.put(now,map.getOrDefault(now,0)+result);
         }
+
+
+        return map.getOrDefault(now,0)+1;
     }
 
 }
