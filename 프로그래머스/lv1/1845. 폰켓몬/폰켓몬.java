6242c91081a992e62f1bd5beecbd600872c0b1b5
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-20 20:24:48 +0900

    [level 1] Title: 폰켓몬, Time: 4.28 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        return Math.min(set.size(), nums.length / 2);
+    }
+}
