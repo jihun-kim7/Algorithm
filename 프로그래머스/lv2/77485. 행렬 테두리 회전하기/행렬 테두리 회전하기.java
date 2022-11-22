dbfa0a9067e0fb39153d095134bd4b06e334b31d
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-23 01:17:35 +0900

    [level 2] Title: 행렬 테두리 회전하기, Time: 14.99 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ 프로그래머스/lv2/77485. 행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,54 +1,48 @@
-import java.io.*;
-import java.util.*;
-
 class Solution {
-    static int[][] table;
-    static int minVal;
-
+    static int map[][];
     public int[] solution(int rows, int columns, int[][] queries) {
-        table = new int[rows + 1][columns + 1];
-        int count = 0;
-        for (int i = 1; i < rows + 1; i++) {
-            for (int j = 1; j < columns + 1; j++) {
-                ++count;
-                table[i][j] = count;
+        int[] answer = new int[queries.length];
+
+        map=new int[rows+1][columns+1];
+        int idx=1;
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=columns; j++){
+                map[i][j]=idx++;
             }
         }
-        int[] answer = new int[queries.length];
-        int i = 0;
-        for (int[] q : queries) {
-            minVal = 10000;
-            rotate(q[0], q[1], q[2], q[3]);
-            answer[i++] = minVal;
-        }
+        for(int i=0; i<queries.length; i++){
 
+            answer[i]=rotate(queries[i][0],queries[i][1],queries[i][2],queries[i][3]);
+
+        }
 
         return answer;
-    }
 
-    static void rotate(int sr, int sc, int er, int ec) {
-        Queue<Integer> q = new LinkedList<>();
 
-        q.offer(table[sr][sc]);
-        for (int j = sc + 1; j < ec; j++) {//맨윗줄
-            q.offer(table[sr][j]);
-            table[sr][j] = q.remove();
-            minVal = Math.min(minVal, table[sr][j]);
+    }static int rotate(int x1,int y1,int x2,int y2){
+        int x=x1;
+        int y=y1;
+        int[]dx={0,-1,0,1};
+        int[]dy={1,0,-1,0};
+        int dir=3;
+        int temp=map[x][y];
+        int min=temp;
+        while(true){
+            if(x==x2&&y==y1){
+                dir=0;
+            }
+             if(x==x2&&y==y2)dir=1;
+             if(x==x1&&y==y2)dir=2;
+            map[x][y]=map[x+dx[dir]][y+dy[dir]];
+            x+=dx[dir];
+            y+=dy[dir];
+            min=Math.min(map[x][y],min);
+            if(x==x1&&y==y1){
+                map[x1][y1+1]=temp;
+                break;
+            }
         }
-        for (int i = sr; i < er; i++) {//우
-            q.offer(table[i][ec]);
-            table[i][ec] = q.remove();
-            minVal = Math.min(minVal, table[i][ec]);
-        }
-        for (int j = ec; j > sc; j--) {//하
-            q.offer(table[er][j]);
-            table[er][j] = q.remove();
-            minVal = Math.min(minVal, table[er][j]);
-        }
-        for (int i = er; i >= sr; i--) {//좌
-            q.offer(table[i][sc]);
-            table[i][sc] = q.remove();
-            minVal = Math.min(minVal, table[i][sc]);
-        }
+
+        return min;
     }
 }
