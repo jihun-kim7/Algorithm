c091a60f1fcedb9c14b6911f5c7f1d5a23b20709
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-29 23:30:35 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 51.83 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/lv3/77486. 다단계 칫솔 판매/README.md
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/README.md
@@ -1,10 +1,10 @@
 # [level 3] 다단계 칫솔 판매 - 77486 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/77486?language=java) 
 
 ### 성능 요약
 
-메모리: 20.6 MB, 시간: 152.28 ms
+메모리: 136 MB, 시간: 51.83 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        int[] answer = new int[enroll.length];
+        HashMap<String,String> referralMap = new HashMap<>();
+        HashMap<String,Integer> moneyMap = new HashMap<>();
+        for(int i=0; i<enroll.length; i++) {
+            referralMap.put(enroll[i],referral[i]);
+            moneyMap.put(enroll[i],0);
+        }
+
+        moneyMap.put("-",0);
+
+        for(int i=0; i< seller.length; i++) {
+            dfs(seller[i],amount[i]*100,referralMap,moneyMap);
+        }
+
+        for (int i = 0; i < enroll.length; i++) {
+            answer[i] = moneyMap.get(enroll[i]);
+        }
+
+        return answer;
+    }
+
+    public void dfs(String name, Integer sales, HashMap<String,String> referralMap, HashMap<String,Integer> moneyMap) {
+        int dividend = sales / 10;
+        if (name.equals("-") || dividend == 0) {
+            moneyMap.put(name,moneyMap.get(name)+sales);
+            return;
+        }
+        moneyMap.put(name, moneyMap.get(name) + sales - dividend);
+        dfs(referralMap.get(name), dividend, referralMap, moneyMap);
+    }
+}
