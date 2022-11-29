588b3a9d78ff738050f8f87cad6d6792e88bb6e1
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-30 05:27:19 +0900

    [level 1] Title: 최소직사각형, Time: 1.53 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -2,18 +2,9 @@ class Solution {
     public int solution(int[][] sizes) {
         int max = 0;
         int min = 0;
-        for(int[] size : sizes) {
-            int big;
-            int small;
-            if (size[0] > size[1]) {
-                big = size[0];
-                small = size[1];
-            } else {
-                big = size[1];
-                small = size[0];
-            }
-            if (big > max) max = big;
-            if (small > min) min = small;
+        for(int[] size : sizes) {       
+            max = Math.max(max,Math.max(size[0],size[1]));
+            min = Math.max(min,Math.min(size[0],size[1]));
         }
         
         return max*min;
