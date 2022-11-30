4b93509d6d4d0860712508adedd8d400842c3c43
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-30 19:42:56 +0900

    [level 2] Title: H-Index, Time: 0.48 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.java
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -3,18 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         Arrays.sort(citations);
-        int n = citations.length;
-        int answer = 0;
-        
-        for (int i=n-1; i>=0; i--) {
-            if (citations[i] <= n-i) {
-                answer = Math.max(citations[i],answer);
-                break;
-            } else {
-                answer = n-i;
-            }
+
+        int max = 0;
+        for(int i = citations.length-1; i > -1; i--){
+            int min = (int)Math.min(citations[i], citations.length - i);
+            if(max < min) max = min;
         }
 
-        return answer;
+        return max;
     }
 }
