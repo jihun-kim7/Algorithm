7b80a5fec62f6f8b58b6c2aa46dddb70b0dd9389
Jihun Kim <55127200+rhldkszns97@users.noreply.github.com>
2022-11-30 17:57:22 +0900

    [level 2] Title: 가장 큰 수, Time: 3.77 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.java
@@ -4,8 +4,7 @@ class Solution {
     public String solution(int[] numbers) {
         String[] str = Arrays.stream(numbers).mapToObj(String::valueOf).toArray(String[]::new);    
         Arrays.sort(str,(s1,s2) -> (s2+s1).compareTo(s1+s2));    
-        if(str[0].equals("0")) return "0";
-        
+        if(str[0].equals("0")) return "0";    
         return String.join("",str);
     }
 }
