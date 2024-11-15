2f65c23a7626a911d34f8d70d9ebc0355d94708a
Jihun Kim <55127200+jihun-kim7@users.noreply.github.com>
2024-11-15 17:20:57 +0900

    [level 2] Title: 광물 캐기, Time: 0.99 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,45 +1,85 @@
 import java.util.*;
 
 class Solution {
-    // 백트래킹
-    // 곡괭이 순서: 다이아몬드 -> 철 -> 돌 순서로 실행한다. 좋은거 못쓰면 손해이므로 좋은것부터 소모한다.
-
-    int answer;
-    List<Integer> answerList = new ArrayList<>();
-
     public int solution(int[] picks, String[] minerals) {
-        answer = 0;
-        backTracking(picks, minerals, 0);
-        answer = answerList.stream().min(Integer::compareTo).orElse(0);
+         int answer = 0;
 
-        return answer;
-    }
+        int dia = picks[0];
+        int iron = picks[1];
+        int stone = picks[2];
 
-    public void backTracking(int[] picks, String[] minerals, int depth) {
-        if (depth == minerals.length || Arrays.stream(picks).allMatch(i -> i == 0)) {
-            answerList.add(answer);
-            return;
+        // 곡갱이 총 개수
+        int totalPick = dia + iron + stone;
+
+        // 광석 총 개수
+        int n = minerals.length;
+
+        int[][] mineralGroup = new int[totalPick][6];
+
+        // 5개 기준으로 피로도 계산
+        for (int i = 0; i < Math.min(totalPick * 5, n); i += 5) {
+            int sum = 0;
+            int damage = 0;
+
+            for (int j = i; j < Math.min(i + 5, n); j++) {
+                switch (minerals[j]) {
+                    case "diamond":
+                        damage = 25;
+                        break;
+                    case "iron":
+                        damage = 5;
+                        break;
+                    case "stone":
+                        damage = 1;
+                        break;
+                }
+
+                sum += damage;
+                mineralGroup[i / 5][j % 5 + 1] = damage;
+            }
+
+            mineralGroup[i / 5][0] = sum;
         }
 
-        for (int i = 0; i < 3; i++) {
-            int pick = picks[i];
-            if (pick == 0) continue;
-            int sum = 0;
-            int min = Math.min(5, minerals.length - depth);
-            for (int j = 0; j < min; j++) {
-                if (minerals[depth+j].equals("diamond")) {
-                    sum += (i == 0 ? 1 : (i == 1 ? 5 : 25));
-                } else if (minerals[depth+j].equals("iron")) {
-                    sum += (i == 0 || i == 1 ? 1 : 5);
-                } else {
-                    sum += 1;
+        // 피로도 합 기준으로 내림차순 정렬
+        Arrays.sort(mineralGroup, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o2[0] - o1[0];
+            }
+        });
+
+        for (int i = 0; i < totalPick; i++) {
+            int[] temp = mineralGroup[i];
+            String pick = "";
+
+            if (dia > 0) {
+                dia -= 1;
+                pick = "diamond";
+            } else if (iron > 0) {
+                iron -= 1;
+                pick = "iron";
+            } else if (stone > 0) {
+                stone -= 1;
+                pick = "stone";
+            }
+
+			// 광물별 피로도 계산 
+            for (int j = 1; j < 6; j++) {
+                switch (pick) {
+                    case "diamond":
+                        answer += (int) Math.ceil(temp[j] / 25.0);
+                        break;
+                    case "iron":
+                        answer += (int) Math.ceil(temp[j] / 5.0);
+                        break;
+                    case "stone":
+                        answer += temp[j];
+                        break;
                 }
             }
-            picks[i] -= 1;
-            answer += sum;
-            backTracking(picks, minerals, depth + min);
-            picks[i] += 1;
-            answer -= sum;
         }
+
+        return answer;
     }
 }
