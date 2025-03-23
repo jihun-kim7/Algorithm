fb9c3d3be3305c9112acc93196dd4174835bbb0c
Jihun Kim <55127200+jihun-kim7@users.noreply.github.com>
2025-03-23 22:51:23 +0900

    [level 3] Title: 등대, Time: 100.81 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/3/133500. 등대/등대.java
+++ 프로그래머스/3/133500. 등대/등대.java
@@ -1,43 +1,38 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 class Solution {
-    List<Integer>[] graph;
-    int[][] dp;
-    boolean[] visited;
-
     public int solution(int n, int[][] lighthouse) {
-        graph = new ArrayList[n + 1];
-        dp = new int[n + 1][2];
-        visited = new boolean[n + 1];
-
-        // 그래프 초기화
-        for (int i = 1; i <= n; i++) {
-            graph[i] = new ArrayList<>();
+         int answer = 0;
+        List<Set<Integer>> list = new ArrayList<>();
+        for(int idx = 0; idx <= n; idx++) {
+            list.add(new HashSet<>());
         }
-
-        for (int[] edge : lighthouse) {
-            int a = edge[0];
-            int b = edge[1];
-            graph[a].add(b);
-            graph[b].add(a);
+        for(int[] lh : lighthouse) {
+            list.get(lh[0]).add(lh[1]);
+            list.get(lh[1]).add(lh[0]);
         }
+        int[] visit = new int[n + 1];
+        dfs(list, 1, visit);
 
-        dfs(1); // 루트 노드는 1로 설정
-
-        // 루트를 켜거나 안 켤 수 있으므로 둘 중 최솟값 선택
-        return Math.min(dp[1][0], dp[1][1]);
+        for(int v : visit) {
+            if(v == 2) {
+                answer++;
+            }
+        }
+        return answer;
     }
-
-    private void dfs(int node) {
-        visited[node] = true;
-        dp[node][0] = 0; // node를 켜지 않은 경우
-        dp[node][1] = 1; // node를 켠 경우
-
-        for (int child : graph[node]) {
-            if (!visited[child]) {
-                dfs(child);
-                dp[node][0] += dp[child][1]; // 내가 꺼져 있으면 자식은 반드시 켜야 한다
-                dp[node][1] += Math.min(dp[child][0], dp[child][1]); // 내가 켜져 있으면 자식은 자유롭게
+    public void dfs(List<Set<Integer>> list, int pos, int[] visit) {
+        visit[pos] = 1;
+        Set<Integer> set = list.get(pos);
+        for(int way : set) {
+            if(visit[way] == 0) {
+                dfs(list, way, visit);
+                if(visit[way] == 1) {
+                    visit[pos] = 2;
+                }
             }
         }
     }
